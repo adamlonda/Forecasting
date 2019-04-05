174274f0d0d5a3533e43950414edb45ae776cc64
Adam Londa <adamlonda@gmail.com>
2019-04-05 17:20:26 +0200

    Empty project

---
+++ Forecasting/Forecasting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecasting
+//
+//  Created by Adam Londa on 05/04/2019.
+//  Copyright © 2019 Adam Londa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
