75548737ce9835866cff81f8e55bfe2bcd35a9b3
Adam Londa <adamlonda@gmail.com>
2019-04-09 19:59:33 +0200

    Model

---
+++ Forecasting/Model/GpsCoords.swift
@@ -0,0 +1,17 @@
+//
+//  GpsCoords.swift
+//  Forecasting
+//
+//  Created by Adam Londa on 09/04/2019.
+//  Copyright © 2019 Adam Londa. All rights reserved.
+//
+
+struct GpsCoords {
+    let latitude: Float
+    let longitude: Float
+    
+    init(latitude: Float, longitude: Float) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
