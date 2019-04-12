fd7c5b699ff13cf5c672db258d5db33021177811
Adam Londa <adamlonda@gmail.com>
2019-04-12 19:52:34 +0200

    Current weather model

---
+++ Forecasting/Model/CurrentWeather.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentWeather.swift
+//  Forecasting
+//
+//  Created by Adam Londa on 12/04/2019.
+//  Copyright © 2019 Adam Londa. All rights reserved.
+//
+
+struct CurrentWeather {
+    let locationName: String
+    
+    init(locationName: String) {
+        self.locationName = locationName
+    }
+}
