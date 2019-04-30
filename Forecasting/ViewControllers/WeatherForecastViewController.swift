1d671b0c452838fd4bdd369ca926ecfef8a28a20
Adam Londa <adamlonda@gmail.com>
2019-04-30 21:09:36 +0200

    WIP: Forecast UI

--- Forecasting/ViewControllers/WeatherForecastViewController.swift
+++ Forecasting/ViewControllers/WeatherForecastViewController.swift
@@ -9,9 +9,13 @@
 import RxSwift
 import UIKit
 
-class WeatherForecastViewController: ViewControllerBase {
+class WeatherForecastViewController: ViewControllerBase, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+    
     var locationService: LocationProtocol?
     var weatherService: WeatherForecastProtocol?
+    
+    var weatherForecast: WeatherForecast?
     
     private func getWeatherForecast(latitude: Double, longitude: Double) -> Observable<WeatherForecast> {
         guard self.weatherService != nil else {
@@ -28,7 +32,8 @@ class WeatherForecastViewController: ViewControllerBase {
             return self.getWeatherForecast(latitude: $0.latitude, longitude: $0.longitude)
         }).subscribe(
             onNext: { forecast in
-                print("Got the forecast")
+                self.weatherForecast = forecast
+                self.tableView.reloadSections(IndexSet(integer: 0), with: .none)
         },
             onError: { error in
                 self.presentNetworkError()
@@ -37,5 +42,21 @@ class WeatherForecastViewController: ViewControllerBase {
         _ = locationService?.errorFeed.subscribe(onNext: { _ in
             self.presentGeolocationError()
         })
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        fatalError("Not implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fatalError("Not implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        fatalError("Not implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        fatalError("Not implemented")
     }
 }
