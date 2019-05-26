9060ac9daeb99253e1b38257e63c8704597fa05a
Unknown <adamlonda@gmail.com>
2019-05-26 17:16:38 +0200

    WIP: Decodable

--- Forecasting/Model/CurrentWeather.swift
+++ Forecasting/Model/CurrentWeather.swift
@@ -9,11 +9,49 @@
 struct CurrentWeather: Decodable {
     enum CurrentWeatherKeys: String, CodingKey {
         case locationName = "name"
+        case weatherInfo = "weather"
+    }
+    
+    struct WeatherInfo: Decodable {
+        enum WeatherInfoKeys: String, CodingKey {
+            case description = "main"
+            case icon = "icon"
+        }
+        
+        let description: String
+        let icon: String
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: WeatherInfoKeys.self)
+            let description = try container.decode(String.self, forKey: .description)
+            let icon = try container.decode(String.self, forKey: .icon)
+            self.init(description: description, icon: icon)
+        }
+        
+        init(description: String, icon: String) {
+            self.description = description
+            self.icon = icon
+        }
     }
     
     let locationName: String
-    let icon: String
-    let description: String
+    
+    private let weatherInfo: WeatherInfo
+//    let icon: String
+//    let description: String
+    
+    var icon: String {
+        get {
+            return weatherInfo.icon
+        }
+    }
+    
+    var description: String {
+        get {
+            return weatherInfo.description
+        }
+    }
+    
     let temperatureKelvin: Float
     let humidity: Int
     let precipitation: Int?
@@ -23,14 +61,25 @@ struct CurrentWeather: Decodable {
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CurrentWeatherKeys.self)
-        let locationName: String = try container.decode(String.self, forKey: .locationName)
+        let locationName = try container.decode(String.self, forKey: .locationName)
+        let weatherInfo = try container.decode(WeatherInfo.self, forKey: .weatherInfo)
         fatalError("Not implemented")
     }
     
-    init(locationName: String, icon: String, description: String, temperatureKelvin: Float, humidity: Int, precipitation: Int?, pressure: Int, windSpeed: Float, windDegrees: Int) {
+    init(locationName: String,
+         weatherInfo: WeatherInfo,
+//         icon: String,
+//         description: String,
+         temperatureKelvin: Float,
+         humidity: Int,
+         precipitation: Int?,
+         pressure: Int,
+         windSpeed: Float,
+         windDegrees: Int) {
         self.locationName = locationName
-        self.icon = icon
-        self.description = description
+        self.weatherInfo = weatherInfo
+//        self.icon = icon
+//        self.description = description
         self.temperatureKelvin = temperatureKelvin
         self.humidity = humidity
         self.precipitation = precipitation
